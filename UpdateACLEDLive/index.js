8cabf4b038cda45ef3770764b545c303fd2407a1
Adam Pfister <apfister@esri.com>
2019-08-12 16:21:36 -0500

    updates

--- UpdateACLEDLive/index.js
+++ UpdateACLEDLive/index.js
@@ -2,15 +2,12 @@ require('isomorphic-fetch');
 require('isomorphic-form-data');
 
 const moment = require('moment');
-const featureService = require('@esri/arcgis-rest-feature-service');
+const { deleteFeatures, addFeatures } = require('@esri/arcgis-rest-feature-layer');
 const restAuth = require('@esri/arcgis-rest-auth');
 
 const liveFeatureServiceUrl = 'https://services.arcgis.com/LG9Yn2oFqZi5PnO5/arcgis/rest/services/Armed_Conflict_Location_Event_Data_ACLED/FeatureServer/0';
 
-let _SESSION;
-let _ACLEDDATA;
-
-const translateToFeatureJson = (data) => {
+const translateToFeatureJson = data => {
   return data.map(event => {
     return {
       geometry: {
@@ -53,9 +50,43 @@ const translateToFeatureJson = (data) => {
   });
 };
 
-const getLiveAcledData = () => {
-  const fourteenDaysAgo = moment().subtract(14, 'days').format('YYYY-MM-DD');
-  const apiUrl = `https://api.acleddata.com/acled/read?event_date=${fourteenDaysAgo}&event_date_where=%3E=&limit=0&terms=accept`;
+const deleteLiveFeatures = async session => {
+  const deleteParams = {
+    url: liveFeatureServiceUrl,
+    params: { where: '1=1' },
+    authentication: session
+  };
+  return deleteFeatures(deleteParams);
+};
+
+const insertLiveFeatures = (newData, session) => {
+  console.log('inserting features ..');
+  const addParams = {
+    url: liveFeatureServiceUrl,
+    features: newData,
+    authentication: session
+  };
+  return addFeatures(addParams);
+};
+
+const initAuth = async () => {
+  return new Promise((resolve, reject) => {
+    let session = null;
+    try {
+      session = new restAuth.UserSession({
+        username: process.env.SERVICE_USER,
+        password: process.env.SERVICE_PASS
+      });
+      resolve(session);
+    } catch (error) {
+      reject(new Error('unable to get authentication setup'));  
+    }
+  });
+};
+
+const getLiveAcledData = async () => {
+  const fourteenDaysAgo = moment('2019-08-01').subtract(14, 'days').format('YYYY-MM-DD');
+  const apiUrl = `https://api.acleddata.com/acled/read?event_date=${fourteenDaysAgo}&event_date_where=%3E=&limit=0&terms=accept`
 
   console.log('requesting data from ACLED API ..');
   console.log(`ACLED API request URL :: ${apiUrl}`);
@@ -66,85 +97,51 @@ const getLiveAcledData = () => {
       if (!responseData) {
         throw new Error('no response data returned from ACLED API');
       } else if (responseData.count === 0 || responseData.data.length === 0) {
-        throw new Error('no features from ACLED API returned. exiting ..');
+        return [];
       } else {
-        _ACLEDDATA = translateToFeatureJson(responseData.data);
-        return Promise.resolve();
+        return translateToFeatureJson(responseData.data);        
       }
     });
 };
 
-const deleteLiveFeatures = () => {
-  console.log('deleting features ..');
-  const deleteParams = {
-    url: liveFeatureServiceUrl,
-    params: { where: '1=1' },
-    authentication: _SESSION
-  };
-  return featureService.deleteFeatures(deleteParams)
-    .catch((error) => {
-      throw new Error(error);
-    });
-};
-
-const insertLiveFeatures = () => {
-  console.log('inserting features ..');
-  const addParams = {
-    url: liveFeatureServiceUrl,
-    adds: _ACLEDDATA,
-    authentication: _SESSION
-  };
-  return featureService.addFeatures(addParams)
-    .catch((error) => {
-      throw new Error(error);
-    });
-};
-
-const initAuth = () => {
-  return new Promise((resolve, reject) => {
-    _SESSION = new restAuth.UserSession({
-      username: process.env.SERVICE_USER,
-      password: process.env.SERVICE_PASS
-    });
-
-    if (!_SESSION) {
-      reject(new Error('unable to get authentication setup'));
-    }
-
-    resolve();
-  });
-};
-
-const processEvent = () => {
-  initAuth()
-    .then(getLiveAcledData)
-    .then(deleteLiveFeatures)
-    .then(insertLiveFeatures)
-    .then(response => {
-      let message = '';
-      if (response && response.addResults) {
-        message = `successfully added ${response.addResults.length}`;
-      } else {
-        message = 'unable to insert features';
-      }
-      return message;
-    })
-    .catch(error => {
-      return error;
-    });
-};
 
 module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
     
-    if (myTimer.IsPastDue)
-    {
+    if (myTimer.IsPastDue) {
         context.log('JavaScript is running late!');
     }
 
     context.log('ACLED Update Initiated', timeStamp);     
 
-    const result = processEvent();
+    const sessionInfo = await initAuth();
 
-    context.log('ACLED Live update completed', result);
+    let newData = null;
+    try {
+      newData = await getLiveAcledData();
+      if (newData.length === 0) {
+        context.log('ACLED Update Completed. No data returned from API'); 
+        return context.done();
+      }
+    } catch (error) {
+      console.log(error); 
+    }
+    
+    let deleteResponse = null;
+    try {
+      deleteResponse = await deleteLiveFeatures(sessionInfo);
+      console.log(`deleted ${deleteResponse.deleteResults.length} features`); 
+    } catch (error) {
+      console.log(error); 
+    }
+
+    let addResponse = null;
+    try {
+      addResponse = await insertLiveFeatures(newData, sessionInfo);
+    } catch (error) {
+      console.log(error); 
+    }
+
+    context.log(`successfully added ${addResponse.addResults.length} features`);
+    context.log('ACLED Live update completed');
 };
